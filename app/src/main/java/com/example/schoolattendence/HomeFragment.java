7188b5f277565d481db6c7f9d84b8dc1e8bef594
Krishna Jade <krishnajade222@gmail.com>
2023-04-26 17:06:18 +0530

    Data for spinner fetched from endpoint, get class and section specific student list from endpoint

--- app/src/main/java/com/example/schoolattendence/HomeFragment.java
+++ app/src/main/java/com/example/schoolattendence/HomeFragment.java
@@ -4,15 +4,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.DateFormat;
 import java.util.Calendar;
 
 public class HomeFragment extends Fragment {
+
+    private Button searchButton;
+    private TableLayout attendanceTable;
+    private Spinner classSpinner;
+    private Spinner sectionSpinner;
+    private String selectedClass, selectedSection;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -32,6 +54,120 @@ public class HomeFragment extends Fragment {
         Calendar calendar = Calendar.getInstance();
         String currentDate = DateFormat.getDateInstance(DateFormat.MEDIUM).format(calendar.getTime());
         DateEditText.setText(currentDate);
+
+        //Get the values of class and section from spinner
+        classSpinner = rootView.findViewById(R.id.spinner_class);
+        sectionSpinner = rootView.findViewById(R.id.spinner_section);
+        TextView classTextview=rootView.findViewById(R.id.text_view_class_label);
+        TextView sectionTextview=rootView.findViewById(R.id.text_view_section_label);
+
+        //Fetch data for class and section spinner using endpoint
+        String endpointUrl = "https://api.trifrnd.in/testing/student/student.php?apicall=readclass";
+        new SpinnerDataFetcher(classSpinner).execute(endpointUrl);
+
+        SpinnerDataFetcherSection spinnerDataFetchersection = new SpinnerDataFetcherSection(sectionSpinner);
+        spinnerDataFetchersection.execute("https://api.trifrnd.in/testing/student/student.php?apicall=readsection");
+
+
+
+        //use selected values of spinners
+        classSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                 selectedClass = (String)adapterView.getItemAtPosition(position);
+                // Use the selectedClass variable in your code
+                classTextview.setText(selectedClass);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+        sectionSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                selectedSection = (String)adapterView.getItemAtPosition(position);
+                // Use the selectedSection variable in your code
+                sectionTextview.setText(selectedSection);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+        // Get references to the search button and the machine table
+        searchButton = rootView.findViewById(R.id.search_button);
+        attendanceTable = rootView.findViewById(R.id.attendance1_table);
+
+        // Set an OnClickListener to the search button
+        searchButton.setOnClickListener(v -> {
+            // Remove all rows except the header row
+            int childCount = attendanceTable.getChildCount();
+            attendanceTable.removeViews(1, childCount - 1);
+            // Make the API call using Volley library
+            String url = "https://api.trifrnd.in/testing/student/student.php?apicall=readall";
+            JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                    response -> {
+                        // Process the JSON response and update the attendance table
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject student = response.getJSONObject(i);
+
+                                String sclass = student.getString("class");
+                                String section = student.getString("section");
+
+                                String rollNo = student.getString("roll_no");
+                                String name = student.getString("stud_name");
+
+                                RadioGroup radioGroup = new RadioGroup(getContext());
+                                radioGroup.setOrientation(RadioGroup.HORIZONTAL);
+
+                                RadioButton presentRadioButton = new RadioButton(getContext());
+                                presentRadioButton.setText("Present");
+                                presentRadioButton.setId(View.generateViewId());
+
+                                RadioButton absentRadioButton = new RadioButton(getContext());
+                                absentRadioButton.setText("Absent");
+                                absentRadioButton.setId(View.generateViewId());
+
+                                radioGroup.addView(presentRadioButton);
+                                radioGroup.addView(absentRadioButton);
+
+                                TableRow.LayoutParams layoutParamsStatus = new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 3f);
+                                radioGroup.setLayoutParams(layoutParamsStatus);
+
+                                TableRow row = new TableRow(getContext());
+                                //only show the students of user selected class and section
+                                if(sclass.equals(selectedClass)&&section.equals(selectedSection)){
+                                    TextView rollNoTextView = new TextView(getContext());
+                                    rollNoTextView.setText(rollNo);
+                                    TableRow.LayoutParams layoutParamsRoll = new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1f);
+                                    rollNoTextView.setLayoutParams(layoutParamsRoll);
+
+                                    TextView nameTextView = new TextView(getContext());
+                                    nameTextView.setText(name);
+                                    TableRow.LayoutParams layoutParamsName = new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 3f);
+                                    nameTextView.setLayoutParams(layoutParamsName);
+                                    nameTextView.setPadding(20,0,0,0);
+
+                                    row.addView(rollNoTextView);
+                                    row.addView(nameTextView);
+                                    row.addView(radioGroup);
+                                }
+
+                                attendanceTable.addView(row);
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    },
+                    Throwable::printStackTrace);
+
+            // Add the request to the RequestQueue
+            RequestQueue queue = Volley.newRequestQueue(getContext());
+            queue.add(request);
+        });
         return rootView;
     }
-}
+    }
---
+++ app/src/main/java/com/example/schoolattendence/SpinnerDataFetcherSection.java
@@ -0,0 +1,98 @@
+package com.example.schoolattendence;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpinnerDataFetcherSection extends AsyncTask<String, Void, List<String>> {
+
+    private Spinner spinner;
+    private ArrayAdapter<String> adapter;
+
+    public SpinnerDataFetcherSection(Spinner spinner) {
+        this.spinner = spinner;
+        adapter = new ArrayAdapter<>(spinner.getContext(), android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    @Override
+    protected List<String> doInBackground(String... urls) {
+        String url = urls[0];
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String jsonStr = null;
+        try {
+            URL requestUrl = new URL(url);
+            urlConnection = (HttpURLConnection) requestUrl.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuilder buffer = new StringBuilder();
+            if (inputStream == null) {
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line).append("\n");
+            }
+            if (buffer.length() == 0) {
+                return null;
+            }
+            jsonStr = buffer.toString();
+        } catch (IOException e) {
+            Log.e("SpinnerDataFetcher", "Error ", e);
+            return null;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e("SpinnerDataFetcher", "Error closing stream", e);
+                }
+            }
+        }
+        try {
+            JSONArray jsonArray = new JSONArray(jsonStr);
+            List<String> items = new ArrayList<>();
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                items.add(jsonObject.getString("division"));
+            }
+            return items;
+        } catch (JSONException e) {
+            Log.e("SpinnerDataFetcher", e.getMessage(), e);
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(List<String> result) {
+        if (result != null) {
+            adapter.clear();
+            adapter.addAll(result);
+            adapter.notifyDataSetChanged();
+        }
+    }
+}
+
